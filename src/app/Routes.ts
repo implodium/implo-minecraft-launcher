e09743f3dfa4cdec948c8c0efc9df806cde13dd0
Quirin <quirin.ecker@bajupa.com>
2021-06-07 15:08:37 +0200

    added login component and set as default component

---
+++ src/app/Routes.ts
@@ -0,0 +1,8 @@
+import {Routes} from "@angular/router";
+import {LoginComponent} from "./components/login/login.component";
+
+const routes: Routes = [
+  {path: '', component: LoginComponent}
+]
+
+export default routes;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import {RouterModule} from "@angular/router";
+import Routes from './Routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(Routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
