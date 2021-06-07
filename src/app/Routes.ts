7c483bf262e717383c286bf455d4c077b2ed0cc0
Quirin <quirin.ecker@bajupa.com>
2021-06-07 18:23:07 +0200

    reimported missing route

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -1,5 +1,6 @@
 import {Routes} from "@angular/router";
 import {MainComponent} from "./components/main/main.component";
+import {LoginComponent} from "./components/login/login.component";
 
 const routes: Routes = [
   {path: '', component: MainComponent},
