84e3f44c649fe86c16b502a163d2f9062d81ba0a
Quirin <quirin.ecker@bajupa.com>
2021-06-16 14:08:39 +0200

    added design for the home screen

--- angular/src/app/components/home/home.component.ts
+++ angular/src/app/components/home/home.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  installed: boolean = false;
+  modPackName: string = 'invalid Name';
+  logoSrc: string = 'img/logo.png';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get logoPath(): string {
+    return `assets/${this.logoSrc}`
   }
 
 }
