14ac04a62149f9819495f6036e2bbd3deb682ae2
Quirin <quirin.ecker@bajupa.com>
2021-06-16 11:42:18 +0200

    added standard configurations for modpack

--- electron/src/control/FileController.ts
+++ electron/src/control/FileController.ts
@@ -15,7 +15,23 @@ export default class FileController {
                 if (!err) {
                     resolve()
                     finished.push(new Promise((resolve, reject) => {
-                        fs.writeFile(this.installPath.relativeToPath('auth.json'), JSON.stringify({loggedIn: false}), err => {
+                        fs.writeFile(this.installPath.relativeToPath('launcher-config.json'), JSON.stringify({
+                            modPacks: {
+                                summer2021: {
+                                    installUrl: "https://drive.google.com/file/d/1Y1Nn7Nh1y_D8-u1-ioJmHHCbzM7WnSlk/view?usp=sharing",
+                                    mineCraftOpt: {
+                                        created: "1970-01-01T00:00:00.000Z",
+                                        gameDir: this.installPath.relativeToPath('instances/sumer2021'),
+                                        icon: "Furnace",
+                                        javaArgs: "-Xmx8G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M",
+                                        lastVersionId: "forge",
+                                        name: "sumer2021",
+                                        type: "custom"
+                                    }
+                                }
+                            },
+                            lastModPack: "summer2021"
+                        }), err => {
                             if (!err) {
                                 resolve()
                             } else {
