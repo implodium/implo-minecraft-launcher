34aa3d742aae3533b53f3478dc5da6a0ad383157
Quirin <quirin.ecker@bajupa.com>
2021-08-06 03:38:55 +0200

    removed debug line

--- electron/src/index.ts
+++ electron/src/index.ts
@@ -3,8 +3,6 @@ import {app} from 'electron'
 import App from "./App";
 import CDIContainer from "./container/CDIContainer";
 
-console.log("starting app")
-
 app.on('ready', () => {
     const app = CDIContainer.resolve(App)
     app.init()
