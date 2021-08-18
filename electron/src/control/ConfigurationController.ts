b073f3f31a88bc4e9c948a46dbd94601848be4de
Quirin <quirin.ecker@bajupa.com>
2021-08-18 17:06:13 +0200

    changed configuration with new release and new name for modpack

--- electron/src/control/ConfigurationController.ts
+++ electron/src/control/ConfigurationController.ts
@@ -12,27 +12,27 @@ export class ConfigurationController {
     public defaultConfiguration: LauncherConfiguration = {
         modPacks: [
             {
-                id: "summer2021",
+                id: "low_on_time",
                 name: '',
                 logo: 'img/low_on_time_logo.png',
-                installUrl: "https://github.com/implodium/minecraft-low-on-time/releases/download/0.5/summer2021.zip",
+                installUrl: "https://github.com/implodium/minecraft-low-on-time/releases/download/1.0/low_on_time.zip",
                 memory: 8,
                 mineCraftOpt: {
                     created: "1970-01-01T00:00:00.000Z",
                     gameDir: this.pathController
                         .installPath
                         .path
-                        .relativeToPath('instances/summer2021'),
+                        .relativeToPath('instances/low_on_time'),
                     icon: "Furnace",
                     javaArgs: "-Xmx8G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M",
                     lastVersionId: "1.12.2-forge-14.23.5.2855",
-                    name: "summer2021",
+                    name: "Low on Time",
                     type: "custom",
                     lastUsed: ""
                 }
             }
         ],
-        lastModPackID: "summer2021"
+        lastModPackID: "low_on_time"
     }
 
     constructor(
